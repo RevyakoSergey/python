7c34763efcf3186e7b2631b3dff31e86b25240b2
sergey <grani2010@yandex.ru>
2022-12-23 22:16:04 +0300

    Добавил файлы дз

---
+++ les3_3.py
@@ -0,0 +1,11 @@
+#Задача 3
+
+def conv_bin(num: int):
+    list_nums = []
+
+    while num > 0:
+        list_nums.insert(0,num % 2)
+        num //= 2
+    print(*list_nums, sep="") 
+
+conv_bin(int(input()))  
