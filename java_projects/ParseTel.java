f643b2e4e05712746dbdf647557f7583a551bde0
sergey <grani2010@yandex.ru>
2023-06-10 00:01:30 +0300

    Добавил дз Иванов

---
+++ java_projects/ParseTel.java
@@ -0,0 +1,26 @@
+package java_projects;
+
+public class ParseTel {
+    String[] arr;
+  
+    public void setArr(String[] arr) {
+      this.arr = arr;
+    }
+  
+    public int getTel() {
+  
+      for (int i = 0; i < arr.length; i++) {
+        try {
+          int telnum= Integer.parseInt(arr[i]);
+          return i;
+        } catch (NumberFormatException e) {
+          if (i == arr.length-1) {
+            
+            System.out.println("Telephone number not parsed!");
+          }
+          
+        }
+      }
+      return -1;
+    }
+  }
